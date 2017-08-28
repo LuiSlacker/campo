001b8f4e9a3a882c3b3ec13b8731337b90e54420
Petulant Slacker <l.goohsen@gmail.com>
2017-08-28 16:05:39 -0500

    added first version

--- app/src/main/java/com/uninorte/campo/MainActivity.java
+++ app/src/main/java/com/uninorte/campo/MainActivity.java
@@ -1,15 +1,25 @@
 package com.uninorte.campo;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static com.uninorte.campo.R.id.fab;
 
 public class MainActivity extends AppCompatActivity {
+
+    static int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +27,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        final LinearLayout mainLayout = (LinearLayout) findViewById(R.id.main_layout);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                MainActivity.counter++;
+                View campo = getLayoutInflater().inflate(R.layout.campo, null);
+                TextView campoLabel = (TextView) campo.findViewById(R.id.campo_label);
+                Button btn = (Button) campo.findViewById(R.id.button);
+
+                campoLabel.setText("Campo "+ MainActivity.counter);
+                btn.setTag(MainActivity.counter + "");
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Log.d("tag", "Campo " + view.getTag() + " clicked");
+                        Toast.makeText(MainActivity.this, "Campo" + view.getTag() +" clicked",
+                                Toast.LENGTH_SHORT).show();
+
+                   }
+                });
+
+                mainLayout.addView(campo);
             }
         });
+
     }
 
     @Override
